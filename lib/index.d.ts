20927eec59a2b53e0869b31a78cd17f3f34ccbe0
Ryan Howard <ryanhoward@igneoussystems.com>
2019-07-31 18:12:32 -0500

    Refactored tests to use TupleIndex

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,8 @@
+export declare enum TupleIndex {
+    State = 0,
+    Publish = 1,
+    Unsubscribe = 2
+}
 export declare function usePubSub<T>(topic: string, defaultState: T): SubscriptionTuple<T>;
 declare type Publish<T> = (newState: T) => void;
 declare type PublicUnsubscribe = () => void;
