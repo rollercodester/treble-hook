43bdf09709158b68fe18e8d0f5f5546a0defa7cf
Ryan Howard <ryanhoward@igneoussystems.com>
2020-04-10 11:02:10 -0500

    2.0.0-alpha.3

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,14 @@
 import React, { Dispatch, SetStateAction } from 'react';
 declare const _default: {
     addTopic<T>(topicName: string, defaultValue: T): void;
-    getPublisher(): React.FunctionComponent<{
+    getPublisher(topics?: string[] | undefined): React.FunctionComponent<{
         children?: React.ReactNode;
     }>;
 };
 export default _default;
 export declare function usePubSub<T>(topic: string): [T, React.Dispatch<React.SetStateAction<T>>];
 export declare type Publish<T> = Dispatch<SetStateAction<T>>;
+export declare type PubSubTuple<T> = [T, Publish<T>];
 export declare enum PubSubTupleIndex {
     State = 0,
     Publish = 1
--- src/index.tsx
+++ src/index.tsx
@@ -11,73 +11,107 @@ import React, {
   useState
 } from 'react'
 
-// topic cache
-const topics: Topic = {}
+const topicsCache: Topic = {}
 
 export default (function TrebleHookPublisherFactory() {
 
-   return {
-      addTopic<T>(topicName: string, defaultValue: T) {
-         if (topics[topicName]) {
-            throw new Error(`The topic "${topicName}" has already been added.`)
-         }
+  return {
 
-         // @ts-ignore
-         const context = createPublishContext<T>()
-         const provider = createPublishProvider<T>(context, defaultValue)
+    /**
+     * Adds a new topic to TrebleHook that can be published and subscribed to.
+     * @param topicName The name of the topic to add.
+     * @param defaultValue The default (initial) state value for the topic.
+     */
+    addTopic<T>(topicName: string, defaultValue: T) {
 
-         topics[topicName] = {
-            context,
-            provider,
-         }
-      },
+      if (topicsCache[topicName]) {
+        throw new Error(`The topic "${topicName}" has already been added.`)
+      }
 
-      getPublisher() {
-         const TrebleHookPublisher: FC<PropsWithChildren<{}>> = ({
-            children,
-         }) => {
-            const ProviderNest = Object.keys(topics).reduce(
-               (tally, topicName) => {
-                  const topic = topics[topicName]
+      // @ts-ignore
+      const context = createPublishContext<T>()
 
-                  if (!topic) {
-                     throw new Error(getNoTopicErrorMessage(topicName))
-                  }
+      const provider = createPublishProvider<T>(context, defaultValue)
 
-                  const Provider = topic.provider
+      topicsCache[topicName] = {
+        context,
+        provider,
+      }
 
-                  return <Provider>{tally}</Provider>
-               },
-               <>{children}</>
-            )
+    },
 
-            return ProviderNest
-         }
+    /**
+     * Returns a TrebleHookPublisher that manages publications for given topics.
+     * The returned publisher should be placed above all consuming components in
+     * React component tree.
+     * @param topics Optional array of topics to publish to. By default,
+     * all added topics will be published to.
+     */
+    getPublisher(topics?: string[]) {
 
-         return TrebleHookPublisher
-      },
-   }
+      const TrebleHookPublisher: FC<PropsWithChildren<{}>> = ({
+        children,
+      }) => {
+
+        //
+        // don't trust the topics passed in...always defensively
+        // build list to publish by filtering from actual cache
+        //
+        const topicNames = topics && topics.length
+          ? Object.keys(topicsCache).filter(key => topics.some(topicName => topicName === key))
+          : Object.keys(topicsCache)
+
+        const ProviderNest = topicNames.reduce((tally, topicName) => {
+
+          const topic = topicsCache[topicName]
+
+          if (!topic) {
+            throw new Error(getNoTopicErrorMessage(topicName))
+          }
+
+          const Provider = topic.provider
+
+          return <Provider>{tally}</Provider>
+
+        }, <>{children}</>)
+
+        return ProviderNest
+
+      }
+
+      return TrebleHookPublisher
+
+    },
+
+  }
+
 })()
 
+/**
+ * Hook that subscribes to a topic
+ * @param topic The topic to subscribe to.
+ */
 export function usePubSub<T>(topic: string) {
-   if (!topics[topic]) {
-      throw new Error(getNoTopicErrorMessage(topic))
-   }
 
-   const topicDef = topics[topic]
+  if (!topicsCache[topic]) {
+    throw new Error(getNoTopicErrorMessage(topic))
+  }
 
-   const context = useContext<ContextState<T>>(
-      topicDef.context as Context<ContextState<T>>
-   )
+  const topicDef = topicsCache[topic]
 
-   if (!context) {
-      throw new Error(
-         `The "${topic} topic must be used within the context of a TrebleHook publisher.
+  const context = useContext<PubSubTuple<T>>(
+    topicDef.context as Context<PubSubTuple<T>>
+  )
+
+  if (!context) {
+    throw new Error(
+      `The "${topic} topic must be used within the context of a TrebleHook publisher.
          Please wrap your App component with a TrebleHook publisher.`
-      )
-   }
+    )
+  }
 
-   return context
+  return context
+
 }
 
 //
@@ -87,11 +121,14 @@ export function usePubSub<T>(topic: string) {
 //
 
 /**
- * Signature for Publish method
+ * Signature for publish method
  */
 export type Publish<T> = Dispatch<SetStateAction<T>>
 
-type ContextState<T> = [T, Publish<T>]
+/**
+ * Signature for state and publish tuple
+ */
+export type PubSubTuple<T> = [T, Publish<T>]
 
 /**
  * Positional indexes for the tuple that is returned by usePubSub.
@@ -102,10 +139,10 @@ export enum PubSubTupleIndex {
 }
 
 interface Topic {
-   [name: string]: {
-      provider: ElementType
-      context: Context<ContextState<unknown>>
-   }
+  [name: string]: {
+    provider: ElementType
+    context: Context<PubSubTuple<unknown>>
+  }
 }
 
 //
@@ -115,8 +152,11 @@ interface Topic {
 //
 
 function createPublishContext<T>() {
-  // @ts-ignore
-  return createContext<ContextState<T>>()
+  // @ts-ignore because we don't want to
+  // pass in default value, which is really
+  // optional under the hood even though
+  // type definition has it required
+  return createContext<PubSubTuple<T>>()
 }
 
 function createPublishProvider<T>(
@@ -124,8 +164,8 @@ function createPublishProvider<T>(
   defaultValue: T
 ) {
   return (props: ProviderProps<T>) => {
-     const contextState = useState(defaultValue)
-     return <TrebleHookContext.Provider value={contextState} {...props} />
+    const contextState = useState(defaultValue)
+    return <TrebleHookContext.Provider value={contextState} {...props} />
   }
 }
 
